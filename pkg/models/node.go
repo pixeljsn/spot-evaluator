655f0951b4d13eef9132d9ebd2a7818d51a3d848
pixeljsn <153773946+pixeljsn@users.noreply.github.com>
2026-01-23 00:36:17 -0500

    Update node.go

--- pkg/models/node.go
+++ pkg/models/node.go
@@ -1,9 +1,11 @@
 package models
 
 type NodeGroup struct {
-    InstanceType string  `json:"instance_type"`
-    AZ           string  `json:"az"`
-    Region       string  `json:"region"`
-    Count        int     `json:"count"`
-    IsSpot       bool    `json:"is_spot"`
+	InstanceType string  `json:"instance_type"`
+	AZ           string  `json:"az"`
+	Region       string  `json:"region"`
+	Count        int     `json:"count"`
+	IsSpot       bool    `json:"is_spot"`
+	OnDemandPrice float64 `json:"on_demand_price"`
+	SpotPrice     float64 `json:"spot_price"`
 }
